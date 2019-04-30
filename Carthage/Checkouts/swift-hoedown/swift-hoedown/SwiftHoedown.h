29a6a720e63053747843a38bd3c5e19c110c3a74
iLifexiao <710178950@qq.com>
2019-04-30 21:48:36 +0800

    更新修改

---
+++ Carthage/Checkouts/swift-hoedown/swift-hoedown/SwiftHoedown.h
@@ -0,0 +1,16 @@
+//
+//  swift-hoedown.h
+//  swift-hoedown
+//
+//  Created by Niels de Hoog on 04/05/15.
+//  Copyright (c) 2015 Invisible Pixel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for swift-hoedown.
+FOUNDATION_EXPORT double swift_hoedownVersionNumber;
+
+//! Project version string for swift-hoedown.
+FOUNDATION_EXPORT const unsigned char swift_hoedownVersionString[];
+
